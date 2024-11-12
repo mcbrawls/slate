0e98603d827adbf012c9266f1ead41dd4f52dea6
Daniel Orr <orrdaniel73@gmail.com>
2024-11-12 14:55:29 +0000

    feat: move layer to modify function

--- src/main/kotlin/net/mcbrawls/slate/layer/paged/LayerPageSourceListener.kt
+++ src/main/kotlin/net/mcbrawls/slate/layer/paged/LayerPageSourceListener.kt
@@ -4,15 +4,16 @@ package net.mcbrawls.slate.layer.paged
  * Listens to modifications to the internal collection and updates the layer accordingly.
  */
 class LayerPageSourceListener<T>(
-    val layer: PagedSlateLayer,
-    private val _collection: MutableCollection<T> = mutableListOf(),
+    collection: MutableCollection<T>,
 ) {
+    private val _collection: MutableCollection<T> = collection
+
     val collection: Collection<T> get() = _collection.toList()
 
     /**
      * Modify and update the internal collection.
      */
-    fun modify(action: MutableCollection<T>.() -> Unit) {
+    fun modify(layer: PagedSlateLayer, action: MutableCollection<T>.() -> Unit) {
         synchronized(_collection) {
             action.invoke(_collection)
             layer.slotCount = _collection.size
@@ -24,6 +25,6 @@ class LayerPageSourceListener<T>(
         /**
          * Creates a page source listener from this collection.
          */
-        fun <T> MutableCollection<T>.layerPageListener(layer: PagedSlateLayer) = LayerPageSourceListener(layer, this)
+        fun <T> MutableCollection<T>.layerPageListener(layer: PagedSlateLayer) = LayerPageSourceListener(this)
     }
 }
