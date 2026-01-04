691d945ad06824b69112817c9b058b9283e992b6
Daniel Orr <orrdaniel73@gmail.com>
2026-01-04 19:19:26 +0000

    fix: deprecation

--- src/main/kotlin/net/mcbrawls/slate/tile/SuspendedTile.kt
+++ src/main/kotlin/net/mcbrawls/slate/tile/SuspendedTile.kt
@@ -46,7 +46,7 @@ class SuspendedTile(
 
                 // only update for latest function call
                 synchronized(latestCallTimestamp) {
-                    if (timestamp == latestCallTimestamp.value) {
+                    if (timestamp == latestCallTimestamp.get()) {
                         tile = newTile
                         factoryState = FactoryState.FINISHED
                     }
